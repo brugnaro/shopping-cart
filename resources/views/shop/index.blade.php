486193c21e1fcff7cb1a98af0b69a05df2c85843
marcos <marcosrodriguesbrugnaro@gmail.com>
2017-09-09 12:40:31 -0300

    upload initial project

---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.master')
+
+@section('title')
+    Laravel Shopping cart
+@endsection
+
+@section('content')
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://ewedit.files.wordpress.com/2016/09/hpsorcstone.jpg?w=405" alt="..." class="img-responsive">
+      <div class="caption">
+        <h3>Product Title</h3>
+        <p class="description">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de 
+        impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido 
+        pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos</p>
+        <div class="clearfix">
+            <div class="pull-left price">$12</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://ewedit.files.wordpress.com/2016/09/hpsorcstone.jpg?w=405" alt="..." class="img-responsive">
+      <div class="caption">
+        <h3>Product Title</h3>
+        <p class="description">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de 
+        impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido 
+        pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos</p>
+        <div class="clearfix">
+            <div class="pull-left price">$12</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://ewedit.files.wordpress.com/2016/09/hpsorcstone.jpg?w=405" alt="..." class="img-responsive">
+      <div class="caption">
+        <h3>Product Title</h3>
+        <p class="description">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de 
+        impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido 
+        pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos</p>
+        <div class="clearfix">
+            <div class="pull-left price">$12</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
